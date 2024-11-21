31f796bf647eaf4403e8d1cc4e47bfef568a5784
Raph <raph4sure007@gmail.com>
2024-11-21 13:12:17 +0100

    Editing the Elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,11 @@ class MyFirstApp extends StatelessWidget {
       body: SingleChildScrollView(child: Center(child: Column(mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const SizedBox(height: 40),
-        const Text("Welcome to my first flutter web page", style: TextStyle(fontSize: 35),),
+        const Text("Welcome to my first flutter web page", style: TextStyle(fontSize: 35),
+        ),
         const SizedBox(height: 40),
-        ElevatedButton(onPressed: () {print('Am Working');}, child: const Text('Try me please'),),
+        ElevatedButton(onPressed: () {print('Am Working');}, child: const Text('Try me please'),
+        ),
         const SizedBox(height: 40),
         Image.network('https://images.pexels.com/photos/20220512/pexels-photo-20220512/free-photo-of-hotel-and-buildings-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
       ],
